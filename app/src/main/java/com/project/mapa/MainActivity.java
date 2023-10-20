3cd25aaa4e188d6a65826b54ce5a149febe5b0ea
gabriel-picolli <gabriel_picollit@hotmail.com>
2023-10-20 17:28:02 -0300

    Commit inicial

---
+++ app/src/main/java/com/project/mapa/MainActivity.java
@@ -0,0 +1,35 @@
+package com.project.mapa;
+
+import android.os.Bundle;
+import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Adicionar um marcador no mapa
+        LatLng localizacao = new LatLng(-34, 151); // Latitude e longitude do local desejado
+        mMap.addMarker(new MarkerOptions().position(localizacao).title("Marcador no Mapa"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(localizacao));
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+
+}
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+}
